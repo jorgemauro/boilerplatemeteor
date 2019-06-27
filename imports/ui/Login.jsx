eb62f1239cfd05e8d2994b7b1bb9aa4eec682ac6
jorgegoncalves <jorgegoncalves@dcc.ufmg.br>
2019-06-27 15:48:23 -0300

    arrumando

--- imports/ui/Login.jsx
+++ imports/ui/Login.jsx
@@ -1,24 +1,50 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {TextField, Button} from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import validator from 'validator';
+
 export default class Login extends Component {
     state = {
-        email:'',
-        nome:'',
-        pssw:'',
+        email: '',
+        nome: '',
+        pssw: '',
     };
-    login =()=>{
-        Meteor.loginWithPassword(this.state.email,this.state.pssw)
+    login = () => {
+        Meteor.loginWithPassword(this.state.email, this.state.pssw)
     };
+
     render() {
         return (
-            <div style={{display:'flex', justifyContent:'center',alignItems:'center', height:'100vh'}}>
-                <Card style={{ MinWidth:'600px', display:'flex', flexFlow:'column', alignItems:'center', padding:'10px'}}>
-                    <TextField error={validator.isEmail(this.state.email)} helperText={validator.isEmail(this.state.email)||this.state.email===''?'':'E-mail invalido'} onChange={event=>this.setState({email:event.target.value})} label="E-mail" />
-                    <TextField onChange={event=>this.setState({pssw:event.target.value})}  label="Senha" type="password"/>
-                    <Button variant='contained' onClick={this.login} fullWidth>Entrar</Button>
-                    <Button variant='contained' onClick={()=>this.props.history.push('/signup')} fullWidth>Cadastrar</Button>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100vw',
+                height: '100vh',
+                background: 'linear-gradient(211.63deg, #545C6E 22.52%, rgba(255, 255, 255, 0) 85.38%), #2B3343',
+            }}>
+                <Card style={{display:'flex', width:'80%', height:'80%'}}>
+                    <div style={{width:'50%', height:'100%', backgroundColor:"#545c6e", display:'flex', justifyContent:'center', alignItems:'center'}}>
+                        <img style={{width:'50%', height:'100%'}} src='/image/login.svg'/>
+                    </div>
+                    <div  style={{
+                        width: '50%',
+                        display: 'flex',
+                        flexFlow:'column    ',
+                        justifyContent:'center',
+                        alignItems: 'center',
+                        padding: '10px'
+                    }}>
+                        <TextField style={{marginBottom:'5px'}} error={validator.isEmail(this.state.email)}
+                                   helperText={validator.isEmail(this.state.email) || this.state.email === '' ? '' : 'E-mail invalido'}
+                                   onChange={event => this.setState({email: event.target.value})} label="E-mail"
+                        fullWidth/>
+                        <TextField style={{marginBottom:'5px'}} onChange={event => this.setState({pssw: event.target.value})} fullWidth label="Senha"
+                                   type="password"/>
+                        <Button style={{marginBottom:'5px'}} variant='contained' onClick={this.login} fullWidth>Entrar</Button>
+                        <Button style={{marginBottom:'5px'}} variant='contained' onClick={() => this.props.history.push('/signup')}
+                                fullWidth>Cadastrar</Button>
+                    </div>
                 </Card>
             </div>
         );
--- imports/ui/Signup.jsx
+++ imports/ui/Signup.jsx
@@ -1,26 +1,59 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {TextField, Button} from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import validator from 'validator';
+
 export default class Signup extends Component {
     state = {
-        email:'',
-        nome:'',
-        pssw:'',
+        email: '',
+        nome: '',
+        pssw: '',
     };
-signup =()=>{
+    signup = () => {
 
-    Meteor.call('signup',this.state.nome,this.state.email,this.state.pssw,this.state.nome)
-};
+        Meteor.call('signup', this.state.nome, this.state.email, this.state.pssw, this.state.nome)
+    };
+
     render() {
         return (
-            <div style={{display:'flex', justifyContent:'center',alignItems:'center', height:'100vh'}}>
-                <Card style={{ MinWidth:'600px', display:'flex', flexFlow:'column', alignItems:'center', padding:'10px'}}>
-                <TextField error={validator.isEmail(this.state.email)} helperText={validator.isEmail(this.state.email)||this.state.email===''?'':'E-mail invalido'} onChange={event=>this.setState({email:event.target.value})} label="E-mail" />
-                <TextField onChange={event=>this.setState({nome:event.target.value})}  label="Nome" />
-                <TextField onChange={event=>this.setState({pssw:event.target.value})}  label="Senha" type="password"/>
-                <TextField label="confirmação da senha" type="password"/>
-                <Button variant='contained' onClick={this.signup} fullWidth>Cadastrar</Button>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100vw',
+                height: '100vh',
+                background: 'linear-gradient(211.63deg, #545C6E 22.52%, rgba(255, 255, 255, 0) 85.38%), #2B3343',
+            }}>
+                <Card style={{display: 'flex', width: '80%', height: '80%'}}>
+                    <div style={{
+                        width: '50%',
+                        height: '100%',
+                        backgroundColor: "#545c6e",
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}>
+                        <img style={{width: '50%', height: '100%'}} src='/image/welcome.svg'/>
+                    </div>
+                    <div style={{
+                        width: '50%',
+                        display: 'flex',
+                        flexFlow: 'column    ',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        padding: '10px'
+                    }}>
+                        <TextField  style={{marginBottom:'5px'}} error={validator.isEmail(this.state.email)}
+                                   helperText={validator.isEmail(this.state.email) || this.state.email === '' ? '' : 'E-mail invalido'}
+                                   fullWidth
+                                   onChange={event => this.setState({email: event.target.value})} label="E-mail"/>
+                        <TextField  style={{marginBottom:'5px'}} fullWidth
+                                   onChange={event => this.setState({nome: event.target.value})} label="Nome"/>
+                        <TextField style={{marginBottom:'5px'}}  fullWidth onChange={event => this.setState({pssw: event.target.value})} label="Senha"
+                                   type="password"/>
+                        <TextField style={{marginBottom:'5px'}}  fullWidth label="confirmação da senha" type="password"/>
+                        <Button variant='contained' onClick={this.signup} fullWidth>Cadastrar</Button>
+                    </div>
                 </Card>
             </div>
         );
